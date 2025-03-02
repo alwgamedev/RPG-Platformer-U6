57a48932d981bd2d2f2375f14ffb8e5fc2ce89ec
lafawnduh71 <yasserfangirl67@gmail.com>
2025-03-02 17:06:53 -0500

    working on forest

---
+++ Assets/Scripts/Core/Animation & Sprites/ChildSortingLayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RPGPlatformer.Core
+{
+    public class ChildSortingLayer : MonoBehaviour
+    {
+        [SerializeField] bool ignoreParentSortingData;
+        [SerializeField] int orderDelta;
+
+        public void SetSortingData(SortingLayer parentLayer, int parentOrder)
+        {
+            if (ignoreParentSortingData) return;
+
+            if (TryGetComponent(out SpriteRenderer sr))
+            {
+                sr.sortingLayerID = parentLayer.id;
+                sr.sortingOrder = parentOrder + orderDelta;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Animation & Sprites/SortingLayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace RPGPlatformer.Core
+{
+    [ExecuteInEditMode]
+    public class SortingLayerControl : MonoBehaviour
+    {
+        [SerializeField] SortingLayer sortingLayer;
+        [SerializeField] int sortingOrder;
+
+        private void OnValidate()
+        {
+            if (ParentHasControl()) return;
+
+            foreach (var child in GetComponentsInChildren<ChildSortingLayer>())
+            {
+                child.SetSortingData(sortingLayer, sortingOrder);
+            }
+        }
+
+        private bool ParentHasControl()
+        {
+            var parent = transform.parent;
+
+            while (parent != null)
+            {
+                if (parent.TryGetComponent<SortingLayerControl>(out _)) return true;
+                parent = parent.parent;
+            }
+
+            return false;
+        }
+    }
+}
