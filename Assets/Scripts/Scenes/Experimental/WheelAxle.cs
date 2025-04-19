14d41a648a70fa4ed0db92e8dd5a40629d6b1e02
lafawnduh71 <yasserfangirl67@gmail.com>
2025-04-18 20:26:47 -0400

    made a working "pronged wheel" system
    not sure if i will use it for pillbug or not

---
+++ Assets/Scripts/Scenes/Experimental/WheelAxle.cs
@@ -0,0 +1,30 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace RPGPlatformer.Movement
+{
+    public class WheelAxle : MonoBehaviour
+    {
+        [SerializeField] Rigidbody2D axle;
+        [SerializeField] float maxSpeed;//angular speed deg/sec
+        [SerializeField] float torque;
+
+        private void FixedUpdate()
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            { 
+                if (axle.angularVelocity > -maxSpeed)
+                {
+                    axle.AddTorque(-torque);
+                }
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (axle.angularVelocity < maxSpeed)
+                {
+                    axle.AddTorque(torque);
+                }
+            }
+        }
+    }
+}
