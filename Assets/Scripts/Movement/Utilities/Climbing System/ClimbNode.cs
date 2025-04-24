14a1ce9dcffbc1195d57e0e210b6697e52812bf7
lafawnduh71 <yasserfangirl67@gmail.com>
2025-04-24 13:16:31 -0400

    committing before i start the climbing system

---
+++ Assets/Scripts/Movement/Utilities/Climbing System/ClimbNode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace RPGPlatformer.Movement
+{
+    public class ClimbNode : MonoBehaviour
+    {
+        public ClimbNode Higher { get; private set; }
+        public ClimbNode Lower {  get; private set; }
+
+        public void SetAdjacentNodes(ClimbNode higher, ClimbNode lower)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Utilities/Climbing System/ClimbableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace RPGPlatformer.Movement
+{
+    public class ClimbableObject : MonoBehaviour
+    {
+        [SerializeField] ClimbNode[] nodes;//in descending order (0 is highest)
+
+        private void Awake()
+        {
+            Configure();
+        }
+
+        private void Configure()
+        {
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i].SetAdjacentNodes(i > 0 ? nodes[i - 1] : null,
+                    i < nodes.Length - 1 ? nodes[i + 1] : null);
+            }
+        }
+    }
+}
