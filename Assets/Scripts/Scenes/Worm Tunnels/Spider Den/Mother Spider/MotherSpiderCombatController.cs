e81808dd2efd29679499096b0c92bcd9e654b562
lafawnduh71 <yasserfangirl67@gmail.com>
2025-05-03 14:46:26 -0400

    minor tweaks to mother spider

--- Assets/Scripts/Scenes/Worm Tunnels/Spider Den/Mother Spider/MotherSpiderCombatController.cs
+++ Assets/Scripts/Scenes/Worm Tunnels/Spider Den/Mother Spider/MotherSpiderCombatController.cs
@@ -20,11 +20,7 @@ namespace RPGPlatformer.Combat
                 var b = base.GetAimPosition();
                 var c = b - (Vector2)Combatant.EquipSlots[Core.EquipmentSlot.Mainhand].transform.position;
                 var d = c.magnitude;
-                return b + 0.01f * d * Mathf.Sign(c.x) * c.CCWPerp();
-                //if you really want to do this correctly just calculate how much projectile will fall 
-                //due to gravity by the time it reaches base.aimPos's x coord (but then we have to
-                //account for the projectile's linear damping, which we don't have on hand in this context,
-                //so I say good enough as is)
+                return b + 0.015f * d * Mathf.Sign(c.x) * c.CCWPerp();
             }
 
             return base.GetAimPosition();
