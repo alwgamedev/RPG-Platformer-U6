f9b68f784b8dbfd16b224ca9eb656626f9c73d96
lafawnduh71 <yasserfangirl67@gmail.com>
2025-05-23 12:29:57 -0400

    improvements to bouncy mushroom & level design

--- Assets/Scripts/Environment/Hazards/Water/Spring1D.cs
+++ Assets/Scripts/Environment/Hazards/Water/Spring1D.cs
@@ -7,41 +7,29 @@ namespace RPGPlatformer.Environment
         //float springConstant;
         //float dampingFactor;
 
+        //bool hasMoved;
         float displacement;
         float velocity;
-        //float springAcceleration;
         float acceleration;
 
         public float Displacement => displacement;
         public float Velocity => velocity;
-        //public float Acceleration => acceleration;
-
-        //public Spring1D(float springConstant, float dampingFactor)
-        //{
-        //    this.springConstant = springConstant;
-        //    this.dampingFactor = dampingFactor;
-        //}
 
         public void Update(float springConstant, float dampingFactor)
         {
-            displacement += velocity;// * Time.deltaTime;
-            velocity += acceleration;// * Time.deltaTime;
+            displacement += velocity;
+            velocity += acceleration;
             acceleration = -springConstant * displacement - dampingFactor * velocity;
         }
 
         public void ApplyAcceleration(float a)
         {
-            velocity += a;// * Time.deltaTime;
+            velocity += a;
         }
-
-        //public void ApplyImpulse(float dv)
-        //{
-        //    velocity += dv;
-        //}
 
         public void ApplyVelocity(float v)
         {
-            displacement += v;// * Time.deltaTime;
+            displacement += v;
         }
     }
 }
