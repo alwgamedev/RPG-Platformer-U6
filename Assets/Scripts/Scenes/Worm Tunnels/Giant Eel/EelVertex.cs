766290fe4db33f0138f88ac9d845f8a783532942
lafawnduh71 <yasserfangirl67@gmail.com>
2025-05-24 19:17:39 -0400

    starting on giant eel

---
+++ Assets/Scripts/Scenes/Worm Tunnels/Giant Eel/EelVertex.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using RPGPlatformer.Core;
+using RPGPlatformer.Movement;
+
+namespace RPGPlatformer.AIControl
+{
+    public class EelVertex : MonoBehaviour
+    {
+        public float wiggleMax;
+        public float wiggleSpeed;
+        
+        int wiggleDirection;//1 = up, -1 = down
+        float wigglePosition;
+        VisualCurveGuidePoint vcgp;//will be childed to EelVertex, so that EelVertex.pos can be the neutral position
+
+        private void Awake()
+        {
+            vcgp = GetComponentInChildren<VisualCurveGuidePoint>();
+        }
+
+        public void InitializeWiggleDirection(int o)
+        {
+            wiggleDirection = o;
+        }
+
+        //u = body direction (normalized); wiggle will be perpendicular to u
+        //actually u might be direction between two adjecent vertices (v[i-1] & v[i+1])
+        public void UpdateWiggle(Vector2 u, HorizontalOrientation o, float dt)
+        {
+            if (wigglePosition * wiggleDirection > wiggleMax)
+            {
+                wiggleDirection *= -1;
+            }
+
+            wigglePosition += wiggleDirection * wiggleSpeed * dt;
+            vcgp.SetPoint((Vector2)transform.position + ((int)o) * wigglePosition * u.CCWPerp());
+            vcgp.SetTangentDir(-u);
+            //we can either use vcg "tangent weight" to uniformly scale the tangents
+            //or scale them individually
+            //actually a good idea might be to scale tangent with or inversely to wigglePosition
+            //(although this could be unnecessary overkill)
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Worm Tunnels/Giant Eel/GiantEel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace RPGPlatformer.AIControl
+{
+    public class GiantEel : MonoBehaviour
+    {
+        //vertices go from back to front of eel (so v[0] is tail of eel, v[^1] is nose of eel)
+        [SerializeField] EelVertex vertices;
+    }
+}
