03d96144b918d40407aa835f793bb8f25e011d13
lafawnduh71 <yasserfangirl67@gmail.com>
2025-06-25 23:24:45 -0400

    added scene bounds to worm tunnels
    so worm tunnels basically done (just need to add drops and a few more rocks + balancing of xp, damage, hp)

---
+++ Assets/Scripts/Scene Management/Utilities/SceneBounds.cs
@@ -0,0 +1,19 @@
+﻿using RPGPlatformer.Core;
+using UnityEngine;
+
+namespace RPGPlatformer.SceneManagement
+{
+    public class SceneBounds : MonoBehaviour
+    {
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            if (!gameObject.activeInHierarchy) return;
+            var p = GlobalGameTools.Instance.PlayerTransform;
+            if (p && collider.transform == p
+                && !GlobalGameTools.Instance.PlayerIsDead)
+            {
+                GlobalGameTools.Instance.Player.Combatant.Instakill();
+            }
+        }
+    }
+}
