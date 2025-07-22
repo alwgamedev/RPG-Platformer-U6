264fa10541158a34551058d514a22a832dac23d0
lafawnduh71 <yasserfangirl67@gmail.com>
2025-07-22 13:53:40 -0400

    Reworked tunnels entrance

---
+++ Assets/Scripts/Core/Animation & Sprites/PolygonColliderRefresher.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace RPGPlatformer.Core
+{
+    [ExecuteAlways]
+    public class PolygonColliderRefresher : MonoBehaviour
+    {
+        public void GenerateCollider()
+        {
+            GenerateCollider(GetComponent<SpriteRenderer>(), GetComponent<PolygonCollider2D>());
+        }
+
+        public static void GenerateCollider(SpriteRenderer sr, PolygonCollider2D c)
+        {
+            if (sr == null || c == null)
+                return;
+
+            if (sr.sprite == null)
+                return;
+
+            var p = new List<Vector2>();
+            sr.sprite.GetPhysicsShape(0, p);
+            c.points = p.ToArray();
+            PrefabUtility.RecordPrefabInstancePropertyModifications(c);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Editor/PolygonColliderRefresherEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RPGPlatformer.Core.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(PolygonColliderRefresher))]
+    public class PolygonColliderRefresherEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if(GUILayout.Button("Refresh Collider"))
+            {
+                foreach (var t in targets)
+                {
+                    ((PolygonColliderRefresher)t).GenerateCollider();
+                }
+            }
+        }
+    }
+}
